e4a1aa830f2fcaa5a1b27be79592cb2cf6d3fb48
Andrew <herzfeld.andrew@gmail.com>
2018-05-09 13:28:50 +0800

    type casted input float to int for calculations

--- pset1/cash.c
+++ pset1/cash.c
@@ -5,35 +5,41 @@
 
 int main(void)
 {
-    float change_owed;
+    float c;
     //prompt user for input (non-negative)
     do 
     {
-        change_owed = get_float("Change owed: ");    
+        c = get_float("Change owed: ");    
     } 
-    while (change_owed < 0);
+    while (c < 0);
     
+    //convert float to int for calculation
+    int change_owed;
+    {
+        c = c * 100;
+        change_owed = (int) c;
+    }
     //algorithm to determine minimum coins required
     
     int quarters, dimes, nickles, pennies;
-    if (change_owed > 0.25)
+    if (change_owed > 25)
     {
-        quarters = change_owed / 0.25;
-        change_owed = change_owed % 0.25;
+        quarters = change_owed / 25;
+        change_owed = change_owed % 25;
     }
-    if (change_owed > 0.10)
+    if (change_owed > 10)
     {
-        dimes = change_owed / 0.10;
-        change_owed = change_owed % 0.10;
+        dimes = change_owed / 10;
+        change_owed = change_owed % 10;
     }
-    if (change_owed > 0.05)
+    if (change_owed > 5)
     {
-        nickles = change_owed / 0.05;
-        change_owed = change_owed % 0.05;
+        nickles = change_owed / 5;
+        change_owed = change_owed % 5;
     }
-    if (change_owed > 0.01)
+    if (change_owed > 1)
     {
-        pennies = change_owed / 0.01;
+        pennies = change_owed / 1;
     }
     //print the minimum number of coins required
     {
