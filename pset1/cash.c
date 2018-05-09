9c8f12d0048d03716a31a26294888c99fe9f1476
Andrew <herzfeld.andrew@gmail.com>
2018-05-09 14:04:39 +0800

    changed if's to elif's and else

--- pset1/cash.c
+++ pset1/cash.c
@@ -27,17 +27,17 @@ int main(void)
         quarters = change_owed / 25;
         change_owed = change_owed % 25;
     }
-    if (change_owed >= 10)
+    elif (change_owed >= 10)
     {
         dimes = change_owed / 10;
         change_owed = change_owed % 10;
     }
-    if (change_owed >= 5)
+    elif (change_owed >= 5)
     {
         nickles = change_owed / 5;
         change_owed = change_owed % 5;
     }
-    if (change_owed >= 1)
+    else
     {
         pennies = change_owed / 1;
     }
